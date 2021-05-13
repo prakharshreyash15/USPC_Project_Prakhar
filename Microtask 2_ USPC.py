62e8abefc0bc974f9ca556224895cdc474d89f7d
Prakhar Shreyash <64785098+prakharshreyash15@users.noreply.github.com>
2021-05-13 23:09:49 +0530

    Update Microtask 2_ USPC.py

--- Microtask 2_ USPC.py
+++ Microtask 2_ USPC.py
@@ -1,14 +1,9 @@
-# Python3 code to find sum of 
-# Manhattan distance between all 
-# the pairs of given points 
-
-# Return the sum of distance 
-# between all the pair of points. 
+# Python3 code to find sum of Manhattan distance between all the pairs of given points 
+# To return the sum of distance between all the pair of points. 
 def distancesum (x, y, n): 
 	sum = 0
 	
-	# for each point, finding distance 
-	# to rest of the point 
+	# for each point, finding distance to rest of the point 
 	for i in range(n): 
 		for j in range(i+1,n): 
 			sum += (abs(x[i] - x[j]) +
